a8ec3c3a99585f4364ec61ff2e5a52daa3ff234f
Hoang Tuan <hoangtuan@example.com>
2024-04-10 22:54:47 +0700

    add demo

--- src/main/java/com/tuan/springbootfinal/request/user/SignUpReq.java
+++ src/main/java/com/tuan/springbootfinal/request/user/SignUpReq.java
@@ -14,7 +14,8 @@ public class SignUpReq {
     @Pattern(regexp = "^[a-zA-Z0-9]{6,20}$", message = "Username must be 6-20 characters")
     private String username;
     @NotBlank(message = "Password is required")
-    @Pattern(regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\\S+$).{6,20}$", message = "Password must be 6-20 characters,phải có chữ thường, chữ in hoa, số ,ký tự đặc biệt và không có khoảng trắng")    private String password;
+    @Pattern(regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\\S+$).{6,20}$", message = "Password must be 6-20 characters,phải có chữ thường, chữ in hoa, số ,ký tự đặc biệt và không có khoảng trắng")    
+    private String password;
     @NotBlank(message = "Email is required")
     @Pattern(regexp = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,6}$", message = "Email is invalid")
     private String email;
